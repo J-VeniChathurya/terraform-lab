0ec39cadfc1d22aa47cc2fa56e17c822b68ba39a
Veni Chathurya <venichathurya1409@gmail.com>
2026-03-22 07:29:03 +0000

    Day3 Docker container using Terraform

---
+++ day3/README.md
@@ -0,0 +1,10 @@
+Day3 — Managing Containers with Terraform
+
+Steps Performed:
+
+1. Configured Docker provider in Terraform.
+2. Pulled nginx image using Terraform.
+3. Created Docker container and mapped port 80 to 8080.
+4. Verified container using docker ps.
+5. Accessed nginx service in browser.
+6. Used output variable to display service URL.
---
+++ day3/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull nginx image
+resource "docker_image" "nginx_image" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+# Create container
+resource "docker_container" "nginx_container" {
+  name  = "terraform-nginx"
+  image = docker_image.nginx_image.image_id
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+# Output URL
+output "service_url" {
+  value = "http://localhost:8080"
+}
